366d0af9202b678c9fddeace792f103fd3188d91
kyleyannelli <kyannell@oswego.edu>
2022-10-01 18:42:54 -0400

    added profile picture & username to react

--- DB_Engine_Networking/OpenLiberty/src/main/frontend/src/App.js
+++ DB_Engine_Networking/OpenLiberty/src/main/frontend/src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+function httpGetSync(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+
+//"https://cdn.discordapp.com/avatars/" + localStorage.claims.id + "/" + localStorage.claims.avatar + ".webp"
+
 function App() {
+    localStorage.setItem("claims", httpGetSync("http://localhost:9080/api/@me/claims"))
+    let jsonClaims = JSON.parse(localStorage.claims);
+    localStorage.setItem("usernameDisc", jsonClaims.username + "#" + jsonClaims.discriminator)
+    localStorage.setItem("userAvatarUrl", "https://cdn.discordapp.com/avatars/" + jsonClaims.id + "/" + jsonClaims.avatar + ".webp")
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={localStorage.userAvatarUrl} className="App-logo" alt="logo" />
         <p>
-
+            {"Welcome " + localStorage.usernameDisc}
         </p>
       </header>
     </div>
