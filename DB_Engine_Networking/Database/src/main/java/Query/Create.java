21dd7e2859327c4bc99bc4dbe60fd5ddc60541e7
daniel <deickler@oswego.edu>
2022-10-06 19:28:56 -0400

    Updated Create, Read, Update, Delete classes to correctly close connections after performing their SQL operations. Improved exception handling.

--- DB_Engine_Networking/Database/src/main/java/Query/Create.java
+++ DB_Engine_Networking/Database/src/main/java/Query/Create.java
@@ -41,17 +41,14 @@ public class Create {
      * @param updated_at                       The time the message was edited.
      */
     public void message(long discord_id, long authors_discord_id, long channels_text_channel_discord_id, String content, long updated_at) {
-
         //trim to size if necessary (it shouldn't be necessary)
-        if (content.length() > Database.MESSAGE_LIMIT)
-            content = content.trim().substring(0, Database.MESSAGE_LIMIT);
+        if (content.length() > Database.MESSAGE_LIMIT) content = content.trim().substring(0, Database.MESSAGE_LIMIT);
 
         //prepare the SQL statement
-        PreparedStatement statement = database.connection().prepareStatement(
         try(PreparedStatement statement = connection.prepareStatement(
                 "INSERT INTO messages\n" +
                         "\t\t\tVALUES(?, ?, ?, ?, ?)"
-        )){
+        )) {
             statement.setLong(1, discord_id);
             statement.setLong(2, authors_discord_id);
             statement.setLong(3, channels_text_channel_discord_id);
@@ -69,9 +66,8 @@ public class Create {
      *
      * @param discord_id The Discord ID of the author being added
      * @param nickname   The Nickname of the author being added
-     * @throws SQLException an exception that provides information on a database access error or other errors
      */
-    public void author(long discord_id, String nickname) throws SQLException {
+    public void author(long discord_id, String nickname) {
         //trim to size if necessary
         if (nickname.length() > Database.NICKNAME_LIMIT)
             nickname = nickname.trim().substring(0, Database.NICKNAME_LIMIT);
@@ -81,7 +77,6 @@ public class Create {
                 "INSERT INTO authors\n" +
                         "\t\t\tVALUES(?, ?, NULL)"
         )) {
-
             statement.setLong(1, discord_id);
             statement.setString(2, nickname);
 
@@ -97,9 +92,8 @@ public class Create {
      * @param discord_id The Discord ID of the author being added
      * @param nickname   The Nickname of the author being added
      * @param avatar_hash The optional hash of the author's avatar
-     * @throws SQLException an exception that provides information on a database access error or other errors
      */
-    public void author(long discord_id, String nickname, String avatar_hash) throws SQLException {
+    public void author(long discord_id, String nickname, String avatar_hash) {
         //trim to size if necessary
         if (nickname.length() > Database.NICKNAME_LIMIT)
             nickname = nickname.trim().substring(0, Database.NICKNAME_LIMIT);
@@ -124,13 +118,13 @@ public class Create {
      *
      * @param emoji   The emoji being added
      * @param meaning The meaning of the emoji being added
-     * @throws SQLException an exception that provides information on a database access error or other errors
      */
-    public void dictionaryEntry(String emoji, String meaning) throws SQLException {
+    public void dictionaryEntry(String emoji, String meaning) {
         //trim to size if necessary
         if (meaning.length() > Database.MEANING_LIMIT) meaning = meaning.trim().substring(0, Database.MEANING_LIMIT);
         if (emoji.length() > Database.EMOJI_LIMIT) emoji = emoji.trim().substring(0, Database.EMOJI_LIMIT);
 
+        //prepare the SQL statement
         try(PreparedStatement statement = connection.prepareStatement(
                 "INSERT INTO dictionary\n" +
                         "\t\t\tVALUES(?, ?)"
@@ -150,14 +144,12 @@ public class Create {
      * @param message_discord_id The Discord ID of the message being reacted to
      * @param authors_discord_id The Discord ID of the user that reacted
      * @param emoji              The emoji used in the reaction
-     * @throws SQLException an exception that provides information on a database access error or other errors
      */
-    public void reaction(long message_discord_id, long authors_discord_id, String emoji) throws SQLException {
-
+    public void reaction(long message_discord_id, long authors_discord_id, String emoji) {
         //trim to size if necessary
-        if (emoji.length() > Database.EMOJI_LIMIT)
-            emoji = emoji.trim().substring(0, Database.EMOJI_LIMIT);
+        if (emoji.length() > Database.EMOJI_LIMIT) emoji = emoji.trim().substring(0, Database.EMOJI_LIMIT);
 
+        //prepare the SQL statement
         try(PreparedStatement statement = connection.prepareStatement(
                 "INSERT INTO reactions\n" +
                         "\t\t\tVALUES(?, ?, ?)"
@@ -177,10 +169,8 @@ public class Create {
      *
      * @param text_channel_discord_id The Discord ID of the channel
      * @param text_channel_nickname   The Nickname of the channel
-     * @throws SQLException an exception that provides information on a database access error or other errors
      */
-    public void channel(long text_channel_discord_id, String text_channel_nickname) throws SQLException {
-
+    public void channel(long text_channel_discord_id, String text_channel_nickname) {
         //trim if necessary
         if (text_channel_nickname.length() > Database.NICKNAME_LIMIT)
             text_channel_nickname = text_channel_nickname.trim().substring(0, 32);
